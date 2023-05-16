4282b1f56a5cafb07be6a65a43f1bb3780d0c8aa
Foxis <42099678+MrKeke@users.noreply.github.com>
2023-05-16 13:13:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import _ from 'lodash'
 
 export default function solution(content){
-  // BEGIN
-
-  // END
-
+  const data = content.split('\n').flatMap((el)=>el != '' ? [el] : []).slice(1)
+  console.log(`Count: ${data.length}`)
+  const cities = _.uniq(data.map((el)=>el.split(',')).map((el)=>el[7]))
+  console.log(`Cities: ${cities.sort().join(', ')}`)
+  const humidity = data.map((el)=>el.split(',')).map((el)=>el[3])
+  console.log(`Humidity: Min: ${Math.min(...humidity)}, Max: ${Math.max(...humidity)}`)
+  const hottestDay = data.map((el)=>el.split(',')).sort((a,b)=>b[1] -a[1])
+  console.log(`HottestDay: ${hottestDay[0][0]} ${hottestDay[0][7]}`)
+  const temCity = {}
+  const maxTemperature = data.map((el)=>el.split(',')).map(([,max,,,,,,name])=>{temCity[name] === undefined ? temCity[name] = Number(max) : temCity[name] +=Number(max)})
+  console.log(`HottestCity: ${Object.entries(temCity).sort((a,b)=>b[1]-a[1])[0][0]}`)
 }
